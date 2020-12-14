14c0fde02365688e85a26aa053f41d41b1f66d3c
Vladislav <Vladislav4869@gmail.com>
2020-12-14 05:42:36 +0300

    10

--- MPI/Main.cpp
+++ MPI/Main.cpp
@@ -1,6 +1,62 @@
-#include "Task1.cpp"
-#include "Task2.cpp"
+﻿#include <mpi.h>
+#include <iostream>
+#include <cstdlib>
 
-int main(int argc, char* argv[]) { 
-	task1(argc, argv);
-};
+int main(int argc, char** argv) {
+
+    int rank, size, N = 100000, array[N];
+    double startSend, endSend;
+    MPI_Status status;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        for (int i = 0; i < N; i++) {
+            array[i] = rand() % 100;
+        }
+
+        startSend = MPI_Wtime();
+        MPI_Send(array, N, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(array, N, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
+        endSend = MPI_Wtime();
+        printf("Время выполнения | Send | %f\n", endSend - startSend);
+
+        startSend = MPI_Wtime();
+        MPI_Ssend(array, N, MPI_INT, 1, 1, MPI_COMM_WORLD);
+        MPI_Recv(array, N, MPI_INT, 1, 1, MPI_COMM_WORLD, &status);
+        endSend = MPI_Wtime();
+        printf("Время выполнения | SSend | %f\n", endSend - startSend);
+
+        startSend = MPI_Wtime();
+        MPI_Buffer_attach(array, N * size * 2 + MPI_BSEND_OVERHEAD * size * 2);
+        MPI_Bsend(array, N, MPI_INT, 1, 2, MPI_COMM_WORLD);
+        MPI_Recv(array, N, MPI_INT, 1, 2, MPI_COMM_WORLD, &status);
+        endSend = MPI_Wtime();
+        printf("Время выполнения | BSend | %f\n", endSend - startSend);
+
+        startSend = MPI_Wtime();
+        MPI_Rsend(array, N, MPI_INT, 1, 3, MPI_COMM_WORLD);
+        MPI_Recv(array, N, MPI_INT, 1, 3, MPI_COMM_WORLD, &status);
+        endSend = MPI_Wtime();
+        printf("Время выполнения | RSend | %f\n", endSend - startSend);
+
+    }
+    else {
+        MPI_Recv(array, N, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        MPI_Send(array, N, MPI_INT, 0, 0, MPI_COMM_WORLD);
+
+        MPI_Recv(array, N, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+        MPI_Ssend(array, N, MPI_INT, 0, 1, MPI_COMM_WORLD);
+
+        MPI_Recv(array, N, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+        MPI_Buffer_attach(array, N * size * 2 + MPI_BSEND_OVERHEAD * size * 2);
+        MPI_Bsend(array, N, MPI_INT, 0, 2, MPI_COMM_WORLD);
+
+        MPI_Recv(array, N, MPI_INT, 0, 3, MPI_COMM_WORLD, &status);
+        MPI_Rsend(array, N, MPI_INT, 0, 3, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+}
