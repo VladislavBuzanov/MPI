abb57175900839c0f082b04e544fe1125cd47d9e
Vladislav <Vladislav4869@gmail.com>
2020-12-14 05:29:27 +0300

    Добавьте файлы проекта.

---
+++ MPI/Main.cpp
@@ -0,0 +1,6 @@
+#include "Task1.cpp"
+#include "Task2.cpp"
+
+int main(int argc, char* argv[]) { 
+	task1(argc, argv);
+};
---
+++ MPI/Task1.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+#include <mpi.h>
+
+void task1(int argc, char* argv[]) {
+    int size, rank, recv;
+    MPI_Status mpiStatus;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0)
+    {
+        printf("Hello World! | from process %d\n", rank);
+        for (int i = 1; i < size; i++)
+        {
+            MPI_Recv(&recv, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG,
+                MPI_COMM_WORLD, &mpiStatus);
+            printf("Hello World! | from process %d\n", recv);
+        }
+    }
+    else
+    {
+        MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+}
